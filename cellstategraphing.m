d7226f5ac4493bdebc62d604584a2cf102e54961
kmitra05 <kinmitra05@gmail.com>
2018-04-26 10:21:25 -0500

    Final changes

--- cellstategraphing.m
+++ cellstategraphing.m
@@ -11,8 +11,8 @@ for i=1:size(xfpdata,2)
     if2=cell2mat(struct2cell(xfpdata(i).IF2)');
     mif1(i)=mean(if1);
     mif2(i)=mean(if2);
-    sif1(i)=std(if1);
-    sif2(i)=std(if2);
+    sif1(i)=std(if1)/sqrt(length(if1));
+    sif2(i)=std(if2)/sqrt(length(if2));
 end
 % pluri(:,1)=msox2(1:4);
 % pluri(:,2)=
@@ -20,23 +20,28 @@ end
 plumarker(:,1)= [mif1(1) mif1(4) mif1(7)];
 plumarker(:,2)= [mif2(1) mif2(4) mif2(7)];
 plumarker(:,3)= [mif1(2) mif1(5) mif1(8)];
+plumarker(:,4)=[mif2(2) mif2(5) mif2(8)];
 erplu(:,1)=[sif1(1) sif1(4) sif1(7)];
 erplu(:,2)=[sif2(1) sif2(4) sif2(7)];
 erplu(:,3)=[sif2(2) sif2(5) sif2(8)];
-barwitherr(erplu,plumarker)
-xaxis={'Fibroblast','ESI017','iPSCs'};
-set(gca,'xtick',1:9,'xticklabel',xaxis)
-xlabel('Cell type')
-ylabel('Mean fluorescence intensity (a.u)')
-legend('OCT4','SOX2','NANOG')
+erplu(:,4)=[sif2(2) sif2(5) sif2(8)];
+plumarker=plumarker';
+erplu=erplu';
 figure
-fibmarker(:,1)=[mif1(3) mif1(6) mif1(9)];
-fibmarker(:,2)=[mif2(2) mif2(5) mif2(8)];
-erfib(:,1)=[sif1(3) sif1(6) sif1(9)];
-erfib(:,2)=[sif2(2) sif2(5) sif2(8)];
-barwitherr(erfib,fibmarker)
-xaxis={'Fibroblast','ESI017','iPSCs'};
-set(gca,'xtick',1:6,'xticklabel',xaxis)
+barwitherr(erplu,plumarker)
+xaxis={'OCT4','SOX2','NANOG','VIMENTIN'};
+set(gca,'xtick',1:12,'xticklabel',xaxis)
 xlabel('Cell type')
 ylabel('Mean fluorescence intensity (a.u)')
-legend('VIMENTIN','FSP1')
+legend('Fibroblast','ESI017','iPSCs')
+
+% fibmarker(:,1)=[mif1(3) mif1(6) mif1(9)];
+% 
+% erfib(:,1)=[sif1(3) sif1(6) sif1(9)];
+% 
+% barwitherr(erfib,fibmarker)
+% xaxis={'Fibroblast','ESI017','iPSCs'};
+% set(gca,'xtick',1:6,'xticklabel',xaxis)
+% xlabel('Cell type')
+% ylabel('Mean fluorescence intensity (a.u)')
+% legend('FSP1','VIMENTIN')
--- soxdosegraphing.m
+++ soxdosegraphing.m
@@ -6,20 +6,21 @@ for i=1:size(xfpdata,2)
     crspr=cell2mat(struct2cell(xfpdata(i).crispr)');
     msox2(i)=mean(sox2);
     mcrspr(i)=mean(crspr);
-    ssox2(i)=std(sox2);
-    scrspr(i)=std(crspr);
+    ssox2(i)=std(sox2)/sqrt(length(sox2));
+    scrspr(i)=std(crspr)/sqrt(length(crspr));
 end
-data(:,1)=msox2(1:4);
-data(:,3)=mcrspr(1:4);
-data(:,2)=msox2(5:8);
-data(:,4)=mcrspr(5:8);
-erdata(:,1)=ssox2(1:4);
-erdata(:,3)=scrspr(1:4);
-erdata(:,2)=ssox2(5:8);
-erdata(:,4)=scrspr(5:8);
+data(:,1)=[msox2(1) msox2(5) mcrspr(1) mcrspr(5)];
+data(:,2)=[msox2(2) msox2(6) mcrspr(2) mcrspr(6)];
+data(:,3)=[msox2(3) msox2(7) mcrspr(3) mcrspr(7)];
+data(:,4)=[msox2(4) msox2(8) mcrspr(4) mcrspr(8)];
+erdata(:,1)=[ssox2(1) ssox2(5) scrspr(1) scrspr(5)];
+erdata(:,2)=[ssox2(2) ssox2(6) scrspr(2) scrspr(6)];
+erdata(:,3)=[ssox2(3) ssox2(7) scrspr(3) scrspr(7)];
+erdata(:,4)=[ssox2(4) ssox2(8) scrspr(4) scrspr(8)];
+figure
 barwitherr(erdata,data)
-xaxis={'0' '1' '10' '100'};
+xaxis={'SOX2 IF WT ESI' 'SOX2 IF MiRAGE ESI' 'SOX2 RFP WT ESI' 'SOX2 RFP MiRAGE ESI'};
 set(gca,'xtick',1:8,'xticklabel',xaxis)
 xlabel('BMP Dose (ng/ml)')
 ylabel('Mean fluorescence intensity (a.u)')
-legend('SOX2 IF WT ESI','SOX2 IF MiRAGE ESI','SOX2 CRISPR WT ESI','SOX2 CRISPR MiRAGE ESI')
+legend('0','1','10','100')
